8a65ef63e6d14d7058d80ac82abd4001319fd105
Pragya <pragya142018@gmail.com>
2023-05-28 00:15:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import abi from "./contract/chai.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Buy from "./components/Buy";
+import Memos from "./components/Memos";
+//import chai from "./chai.png";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  const [account, setAccount] = useState("None");
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0x1B4a43A9DA841d4dCA9B0ECdD8474CF1df2AF92A";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+          const account = await ethereum.request({
+            method: "eth_requestAccounts",
+          });
+
+          window.ethereum.on("chainChanged", () => {
+            window.location.reload();
+          });
+
+          window.ethereum.on("accountsChanged", () => {
+            window.location.reload();
+          });
+
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const contract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+          );
+          setAccount(account);
+          setState({ provider, signer, contract });
+        } else {
+          alert("Please install metamask");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    connectWallet();
+  }, []);
+  // console.log(state);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#EFEFEF", height: "100%" }}>
+      {/* <img src={chai} className="img-fluid" alt=".." width="100%" /> */}
+      <p
+        class="text-muted lead "
+        style={{ marginTop: "10px", marginLeft: "5px" }}
+      >
+        <small>Connected Account - {account}</small>
+      </p>
+      <div className="container">
+        <Buy state={state} />
+        <Memos state={state} />
+      </div>
     </div>
   );
 }
